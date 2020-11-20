0666f4886fa4965273147ffc4e18ded436203463
david <david@hamlyn>
2020-11-20 13:59:08 +0000

    fix some property names

--- tests/CreateSantaGameTest.ps1
+++ tests/CreateSantaGameTest.ps1
@@ -1,30 +1,31 @@
 function new-santagametest {
     [CmdletBinding()]
     Param(
-        $apipath = "https://localhost:5000"
+        $apipath = "http://localhost:5000"
     )
     $DefParams = @{
         UseBasicParsing = $true
         'ContentType' = 'application/json'
     }
     # new
-    $game = irm "$apipath/new" @DefParams -Method GET
+    $game = irm "$apipath/new" @DefParams -Method POST -Body $(@{name="Test Game $(Get-Random)"} |ConvertTo-Json)
+    $game
 
     # test users
     $names = 1..3 | % { "Test User $_" }
     $names | % {
-        $JsonParams = @{'name' = $_; 'code' = $game.code } | ConvertTo-Json
+        $JsonParams = @{'name' = $_; 'code' = $game.pubkey } | ConvertTo-Json
         irm @DefParams -Method POST -uri "$apipath/user" -Body $JsonParams
     }
 
     # ideas
     1..7 | %{
-        $JsonParams = @{ idea = "Test idea $_"; code = $game.code } | ConvertTo-Json
+        $JsonParams = @{ idea = "Test idea $_"; code = $game.pubkey } | ConvertTo-Json
         irm @DefParams -Method POST -uri "$apipath/idea" -Body $JsonParams
     }
 
     # run game
     irm @DefParams -Method POST -uri "$apipath/game" -Body $(
-        @{ code = $game.code; secret = $game.secret; status=1} | ConvertTo-Json
+        @{ code = $game.pubkey; secret = $game.privkey; state=1} | ConvertTo-Json
     )
 }
